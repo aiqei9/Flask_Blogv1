0e4f1c89b0ef9ca56e2d55c7b809d7a28d568b2e
Manuel Albarracin <map949@gmail.com>
2024-09-24 17:36:33 +0200

    Initial commit

---
+++ forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, URL
+# from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()], render_kw={'autofocus': True})
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()], default="https://imgs.search.brave.com/BW__i2u-_aUDX7WcqOc0ZZIrdXUDN73s-jcnwRqSN8k/rs:fit:1024:704:1/g:ce/aHR0cHM6Ly9zdGF0/aWMwMS5ueXQuY29t/L2ltYWdlcy8yMDEx/LzAxLzE0L2FydHMv/MTRNT1ZJTkctc3Bh/bi9NT1ZJTkctanVt/Ym8uanBn")
+    # body = CKEditorField("Blog Content", validators=[DataRequired()])
+    body = TextAreaField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+# TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()], render_kw={'autofocus': True})
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+# TODO: Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()], render_kw={'autofocus': True})
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+# TODO: Create a CommentForm so users can leave comments below posts
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
